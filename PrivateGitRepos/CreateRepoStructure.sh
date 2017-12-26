01d03476823721f39f8d4faa2f559bba27513cf7
Sarah Nadi <nadi@ualberta.ca>
2017-12-26 14:29:11 -0700

    Repo structure for CMPUT 201-W18

--- PrivateGitRepos/CreateRepoStructure.sh
+++ PrivateGitRepos/CreateRepoStructure.sh
@@ -6,13 +6,23 @@ git clone https://github.com/CMPUT201-W17/$repoName
 
 cd $repoName
 
+#Lab folders
 mkdir Labs
-mkdir Assignments
-echo "#Labs directory">>Labs/README.md
-echo "#Assignments directory">>Assignments/README.md
+echo "#Labs directory\n Please do not change this directory structure. Each lab should go under its respective folder.">>Labs/README.md
+for labNum in 1 2 3 .. 12
+do
+	mkdir Labs/Lab$labNum
+done
 
-git add Assignments/*
-git add Labs/*
-git commit Assignments/ Labs/ -m "Add Labs and assignments directories"
+#Assignment folders
+mkdir Assignments
+echo "#Assignments directory\n Please do not change this directory structure. Each assignment should go under its respective folder.">>Assignments/README.md
+for labNum in 1 2 3
+do
+	mkdir Assignments/Assignment$labNum
+done
+
+git add --all
+git commit -am "Add Labs and assignments directories"
 
 git push
