12dbac65785b57c431a9050ed7afb99e8a37871e
Sarah Nadi <nadi@ualberta.ca>
2017-12-26 11:52:21 -0700

    Remove debugging info after testing with old 2017 repos

--- PrivateGitRepos/collect_assignments.py
+++ PrivateGitRepos/collect_assignments.py
@@ -1,5 +1,5 @@
 '''
-This script collects all student repositories in the organization at a given deadline
+This script collects all student repositories in a given organization at a given deadline
 '''
 
 from github3 import login
@@ -25,22 +25,18 @@ def run(username, tokenFile, organization, deadline):
 	currentDir = os.getcwd()
 
 	for repo in course.repositories():
-		print repo, repo.name
+		print("Cloning" + repo)
 		subprocess.call("git clone " + repo.clone_url, shell=True)
 		os.chdir(currentDir + "/" + repo.name)
-		#subprocess.call("cd " + repo.name, shell=True)
-		print("changed to " + os.getcwd())
 		subprocess.call("git checkout \"`git rev-list master -n 1 --first-parent --before=\"" + deadline + "\"`\"", shell=True);
-		#subprocess.call("cd ..", shell=True)
 		os.chdir(currentDir)
-		print(" back to" + os.getcwd())
 
 
 parser = argparse.ArgumentParser(description='Collect repos')
 parser.add_argument('--username', help='Github username')
 parser.add_argument('--token', help='Github token file')
-parser.add_argument('--deadline', help="Deadline of the given assignment in format \"%Y-%m-%d %H:%M:%S -700\"")
-parser.add_argument('--organization', help="Course organization")
+parser.add_argument('--deadline', help="Deadline of the given assignment in format \"%Y-%m-%d %H:%M:%S -0700\" where -0700 indicates the MST time zone")
+parser.add_argument('--organization', help="Course github organization")
 
 args = parser.parse_args()
 run(args.username, args.token, args.organization,args.deadline)
