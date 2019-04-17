062de042ce0ba58ab4573defd632cffdc340f520
snadi <nadi@ualberta.ca>
2019-04-17 08:59:10 -0600

    Checkout master first

--- push_feedback_to_all.sh
+++ push_feedback_to_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#IMPORTANT: This script assumes that you have already done git checkout master after finishing your grading
+#IMPORTANT: This script will checkout master first which will override any changes to already tracked files
 
 feedback_filename=$1
 
@@ -8,6 +8,7 @@ for dir in */; do
     if [[ -d $dir ]]; then
     	echo "Pushing for $dir"
        	cd $dir
+        git checkout master
         git add $1
         git commit $1 -m "Grade assignment"
         git pull --rebase
